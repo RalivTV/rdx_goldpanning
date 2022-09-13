b3989595223c62f50790231b25a7f0e6b5457467
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-09-13 02:33:41 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -13,18 +13,33 @@ end)
 RegisterNetEvent("golding")
 AddEventHandler("golding", function()
 local xPlayer = RDX.GetPlayerFromId(source)
-local roll = math.random(0,7)
-local callItem = Config.itemList[roll]		
+local roll = math.random(0,10)
+local callItem = Config.itemList[roll]
+local callText = Config.texList[roll]		
 
+ if callItemm == nil then
+     TriggerClientEvent("rdx_pneira:alert",source, "Nothing!", 2)		
+     return
+ else
+     xPlayer.addInventoryItem(callItem, 1)            			
+     TriggerClientEvent("rdx_pneira:alert",source, "You found a "..callItem.."!", 2)
+ end			
+			
+		
+		
+		
+		
+		
+		
     if roll == 3 then 
-     xPlayer.addInventoryItem("item_silvernugget", 1)            
+     xPlayer.addInventoryItem("item_silvernugget", 1) 
      TriggerClientEvent("rdx_pneira:alert",source, "You found a Silver Nugget!", 2)
    elseif roll == 2 then 
      xPlayer.addInventoryItem("smallfish", 1)            
      TriggerClientEvent("rdx_pneira:alert",source, "You found a small fish!", 2)
    elseif roll == 1 then 
      xPlayer.addInventoryItem("item_goldnugget", 1)            
-     TriggerClientEvent("rdx_pneira:alert",source, "You found a Gold Nugget!", 2)
+     TriggerClientEvent("rdx_pneira:alert",source, "You found a "..callItem.."!", 2)
    else
      TriggerClientEvent("rdx_pneira:alert",source, "Nothing!", 2)
     end
