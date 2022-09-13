9605d86ea5488db87122b89cffc8c44d79be9353
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-09-13 02:26:48 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,16 +1,16 @@
 Config = {}
 
 Config.itemList = {
-  [0] = nil,
-  [1] = "item_silvernugget",
-  [2]= "small_fish",
-  [3] = nil,
-  [4] = "item_silvernugget",
-  [5] = "small_fish",
-  [6] = "item_foolsgold",
-  [7] = "item_goldnugget"",
-  [8] = nil,
-  [9] = "item_foolsgold",
+  [0] =  nil,
+  [1] =  "item_silvernugget",
+  [2]=   "small_fish",
+  [3] =  nil,
+  [4] =  "item_silvernugget",
+  [5] =  "small_fish",
+  [6] =  "item_foolsgold",
+  [7] =  "item_goldnugget",
+  [8] =  nil,
+  [9] =  "item_foolsgold",
   [10] = nil,
   }
   
