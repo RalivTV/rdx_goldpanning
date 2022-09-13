de673e6bdebe5a19de0960766820725e9fb23bf9
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-09-13 02:42:08 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -2,19 +2,17 @@ RDX = nil
 TriggerEvent('rdx:getSharedObject', function(obj) RDX = obj end) 
 
 RDX.RegisterUsableItem('item_goldpan', function(source)
-	local xPlayer = RDX.GetPlayerFromId(source)
-      
-                TriggerClientEvent('rdx_pneira:StartPaning', source)
-	TriggerClientEvent("rdx_pneira:alert",source, "used goldpan", 2)
-
+    local xPlayer = RDX.GetPlayerFromId(source)
+    TriggerClientEvent('rdx_pneira:StartPaning', source)
+    TriggerClientEvent("rdx_pneira:alert",source, "used goldpan", 2)
 end)
 
 RegisterNetEvent("golding")
 AddEventHandler("golding", function()
-local xPlayer = RDX.GetPlayerFromId(source)
-local roll = math.random(0,10)
-local callItem = Config.itemList[roll]
-local callText = Config.texList[roll]		
+ local xPlayer = RDX.GetPlayerFromId(source)
+ local roll = math.random(0,10)
+ local callItem = Config.itemList[roll]
+ local callText = Config.texList[roll]		
 
  if callItemm == nil then
      TriggerClientEvent("rdx_pneira:alert",source, "..callText..", 2)		
@@ -22,6 +20,5 @@ local callText = Config.texList[roll]
  else
      xPlayer.addInventoryItem(callItem, 1)
      TriggerClientEvent("rdx_pneira:alert",source, "..callText..", 2)
- end			
-    		
+ end   		
 end)
